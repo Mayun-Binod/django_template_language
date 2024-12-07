222ee271497fee887b1f8b9ce781381581c8ce98
Mayun-Binod <shresthabinod334@gmail.com>
2024-12-07 14:40:53 +0545

    template inheritance  with static files in django

--- templateInheritance/templateInheritance/urls.py
+++ templateInheritance/templateInheritance/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core import views
+admin.site.site_header = "Binod Admin"
+admin.site.site_title = "Binod Admin Portal"
+admin.site.index_title = "Welcome to Binod Researcher Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home),
+    path('about/',views.about),
+    path('cor/',include('course.urls')),
 ]
